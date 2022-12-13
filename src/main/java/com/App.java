38b22aaf8e4de112cd9f2cb7ef39393dba6bac18
punch-bob <rybalkokostyan@gmail.com>
2022-12-14 06:34:15 +0700

    added proxy server start

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -2,6 +2,13 @@ package main.java.com;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // TODO
+        if (args.length != 1) {
+            System.err.println("Port not specified");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+        SOCKS5Server server = new SOCKS5Server("127.0.0.1", port);
+        server.run();
     }
 }
